45455352693bee4fe8eb2edea5cd0c2b3720ab49
Indi Klomp <74660474+kwark69@users.noreply.github.com>
2021-09-22 19:49:45 +0200

    Simpele aboutpagina + connecten van de database en tabel aanmaken

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -8,6 +8,9 @@ class AboutController extends Controller
 {
     public function index()
     {
-        return view('about');
+        $title = "BarbellClub";
+        $paragraph = "Op deze website ga je kennis maken met verschillende trainingschamas en je eigen schemas in elkaar zetten.";
+
+        return view( 'about',compact('title', 'paragraph'));
     }
 }
