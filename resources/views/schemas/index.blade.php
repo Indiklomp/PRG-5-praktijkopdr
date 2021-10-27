83eedad51ed74b56efbe655c89024d5b90f3c69c
Indi Klomp <74660474+kwark69@users.noreply.github.com>
2021-10-27 17:04:00 +0200

    update en delete toegevoegd, read aangepast

--- resources/views/schemas/index.blade.php
+++ resources/views/schemas/index.blade.php
@@ -4,13 +4,22 @@
     <h1> {{$title}}</h1>
 
     <a class="btn btn-success" href="{{ route('schemas.create') }}"> Create New Schema</a>
-    <table>
+    <table class="p-3 mb-2 bg-secondary text-light">
     @foreach($schemas as $schema)
         <tr>
             <td> {{$schema['id']}}</td>
-            <td><a href="/schemas/{{$schema->id}}"> {{$schema['title']}} </a></td>
-            <td> <img src="{{ asset("storage/images/".$schema['image']) }}"  height="300px" width="300px"> </td>
+            <td> {{$schema['title']}} </td>
+            <td> <img src="{{ asset("storage/images/".$schema['image']) }}"  height="100px" width="100px"> </td>
             <td> {{$schema ['excerpt']}}</td>
+            <td>
+                <form action="{{ route('schemas.destroy',$schema) }}" method="POST">
+                    <a class="btn btn-info" href="{{ route('schemas.show',$schema) }}">Show</a>
+                    <a class="btn btn-primary" href="{{ route('schemas.edit',$schema) }}">Edit</a>
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">DESTROY</button>
+                </form>
+            </td>
         </tr>
     @endforeach
     </table>
--- resources/views/schemas/show.blade.php
+++ resources/views/schemas/show.blade.php
@@ -1,15 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <article>
-        <h1>{{ $schema['id'] }}</h1>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Schema</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('schemas.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
 
-        <div>
-            <img src="{{ asset("storage/images/".$schema['image']) }}"  height="300px" width="300px">
-</div>
-</article>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                {{ $schema->title }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <img src="{{ asset("storage/images/".$schema['image']) }}"  height="800px" width="800px">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                {{ $schema->excerpt }}
+            </div>
+        </div>
+    </div>
 
-<a href="/schemas">Go Back</a>
+
+
+
+
+
+{{--    <article>--}}
+{{--        <h1>{{ $schema['id'] }}</h1>--}}
+
+{{--        <div>--}}
+{{--            <img src="{{ asset("storage/images/".$schema['image']) }}"  height="300px" width="300px">--}}
+{{--</div>--}}
+{{--</article>--}}
+
+{{--<a href="/schemas">Go Back</a>--}}
 
 
 @endsection
